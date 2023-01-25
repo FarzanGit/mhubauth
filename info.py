9c7127b2a61b4ffffbfad1981dba608dd22d7639
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2023-01-25 12:36:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '')) 
-API_HASH = environ. get('API_HASH', '') 
-BOT_TOKEN = environ.get('BOT_TOKEN', "") 
+API_ID = int(environ.get('API_ID', '19128968')) 
+API_HASH = environ. get('API_HASH', '2f9e44d3ea2bc84ea1553ed6099fb2ae') 
+BOT_TOKEN = environ.get('BOT_TOKEN', "985710912:AAFltFE7dOsnXkRgDNnyaJwcnvmoxVjeVvI") 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -23,18 +23,25 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "") 
-DATABASE_NAME = environ.get('DATABASE_NAME', "") 
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Tokyo:Tokyo@cluster0.ng8fsk2.mongodb.net/?retryWrites=true&w=majority") 
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0") 
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'movieshd_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
+**Hi, I'm Tokyo Bot**
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", """**{file_name}**(`{file_size}`) 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+കാണാൻ താല്പര്യമുള്ള മറ്റു സിനിമകൾ ഗ്രൂപ്പിൽ ചോദിക്കാം 
+👇👇👇
+💢GROUP      🔽
+1) https://t.me/+WAASkCWDVwMHkY1s
+2) https://t.me/+KOoNcfmnCvo4MmE1
+3) https://t.me/+pYhVs_XYllVlNDI1""")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "")
 CUSTOM_FILE_CAPTION = None if FILE_CAPTION.strip() == "" else FILE_CAPTION
 API_KEY = None if OMDB_API_KEY.strip() == "" else OMDB_API_KEY
